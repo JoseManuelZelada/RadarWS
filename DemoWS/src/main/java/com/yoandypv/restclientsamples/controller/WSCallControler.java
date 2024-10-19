bad39b52f8fb5cf9bf9d17cb61326fd3c0cfd79f
JoseManuelZelada <josemanuel.zelada@outlook.com>
2024-10-19 01:24:39 -0500

    Add files via upload

---
+++ DemoWS/src/main/java/com/yoandypv/restclientsamples/controller/WSCallControler.java
@@ -0,0 +1,33 @@
+package com.yoandypv.restclientsamples.controller;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.yoandypv.restclientsamples.pruebas.PruebaWSConsumer;
+import com.yoandypv.restclientsamples.pruebas.PruebaWSReporte;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("WSCall")
+public class WSCallControler {
+
+    @PostMapping(value = "Exec")
+    public String exec() throws JsonProcessingException {
+        PruebaWSConsumer pruebaWSConsumer = new PruebaWSConsumer();
+        String salida = pruebaWSConsumer.Ejecutar("D","08173966");
+
+        String reporteSentinel = "";
+
+        PruebaWSReporte pruebaWSReporte = new PruebaWSReporte();
+        reporteSentinel = pruebaWSReporte.EjecutarReporte(salida);
+
+
+        return reporteSentinel; ///"Executar from public endpoint";
+    }
+
+
+    @PostMapping(value = "login")
+    public String login(@RequestParam String username, @RequestParam String password)
+    {
+        return "login from public endpoint";
+
+    }
+
+}
